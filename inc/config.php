783489f3df34a8ffb516cfdc708466806667b758
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2024-10-31 09:07:05 +0800

    config

--- inc/config.php
+++ inc/config.php
@@ -4,5 +4,4 @@ $database_username = 'u396323918_root';
 $database_password = '1!vOvvy:6qF7';
 $pdo = new PDO('mysql:host=localhost;dbname=u396323918_tripsociety_db', $database_username, $database_password);
 
-
 ?>
