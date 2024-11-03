b7f4322707778d6e187c86de294892bff17916c6
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2024-11-04 01:14:16 +0800

    add top 5

--- user/api/search/fetch-top-destination.php
+++ user/api/search/fetch-top-destination.php
@@ -4,7 +4,10 @@ include '../../../inc/config.php';
 $sql = 'SELECT l.id, l.location_name, l.image, COALESCE(AVG(r.rating), 0) AS average_rating
         FROM tbl_location l
         LEFT JOIN tbl_review r ON l.id = r.location_id
-        GROUP BY l.id, l.location_name, l.image';
+        GROUP BY l.id, l.location_name, l.image
+        ORDER BY average_rating DESC
+        LIMIT 5'; // Limit the result to the top 5
+
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $locations = $stmt->fetchAll(PDO::FETCH_ASSOC);
