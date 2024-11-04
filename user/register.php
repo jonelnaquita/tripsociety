bb0e0d62d02cce26eb216adf562207ac9081985a
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2024-11-04 10:18:52 +0800

    update to tripsociety

--- user/register.php
+++ user/register.php
@@ -125,19 +125,23 @@
             <div class="row">
               <div class="col">
                 <p>
-                  By participating in Ala'emap: Uniting Wanderers in Exploring the Wonders of Batangas ("Ala'emap"), you
-                  agree to comply with the following terms and conditions. Ala'emap is a collaborative initiative aimed
+                  By participating in TripSociety: Uniting Wanderers in Exploring the Wonders of Batangas
+                  ("TripSociety"), you
+                  agree to comply with the following terms and conditions. TripSociety is a collaborative initiative
+                  aimed
                   at bringing together adventurers and explorers to discover the diverse wonders of Batangas.
                   Participants are expected to conduct themselves in a respectful and responsible manner throughout all
-                  activities organized by Ala'emap. All participants must adhere to local laws, regulations, and
-                  guidelines, and are responsible for their own safety and well-being during Ala'emap events. Ala'emap
+                  activities organized by TripSociety. All participants must adhere to local laws, regulations, and
+                  guidelines, and are responsible for their own safety and well-being during TripSociety events.
+                  TripSociety
                   organizers reserve the right to modify or cancel any event due to unforeseen circumstances or safety
                   concerns.
-                  By participating in Ala'emap, you consent to the use of any photographs, videos, or other media
-                  captured during events for promotional purposes. Ala'emap organizers are not liable for any loss,
-                  injury, or damage incurred during participation in Ala'emap activities. Participants are encouraged to
+                  By participating in TripSociety, you consent to the use of any photographs, videos, or other media
+                  captured during events for promotional purposes. TripSociety organizers are not liable for any loss,
+                  injury, or damage incurred during participation in TripSociety activities. Participants are encouraged
+                  to
                   act in an environmentally conscious manner and to respect the natural beauty and cultural heritage of
-                  Batangas. By registering for Ala'emap events, you acknowledge that you have read, understood, and
+                  Batangas. By registering for TripSociety events, you acknowledge that you have read, understood, and
                   agree to abide by these terms and conditions.
                 </p>
               </div>
